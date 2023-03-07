7daaa247fb936a04228a01f5303630ea5523ff4e
Leo Semenenko <leo@gridpane.com>
2023-03-07 00:57:50 -0600

    test

--- .gpconfig/predeploy-server.sh
+++ .gpconfig/predeploy-server.sh
@@ -4,3 +4,9 @@
 # - Runs before files have been deployed
 # - Runs as root user
 # - Runs from within site /.gpconfig dir
+
+timeout 300 wget https://getcomposer.org/download/2.5.4/composer.phar
+timeout 300 chmod 755 ../wp-content/plugins/breakdance/plugin/vendor/bin/mozart
+export COMPOSER_ALLOW_SUPERUSER=1
+timeout 300 php composer.phar update -n --working-dir=../wp-content/plugins/breakdance/plugin/
+timeout 300 php composer.phar install -n --working-dir=../wp-content/plugins/breakdance/plugin/
