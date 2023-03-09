727a0fa2f2d038f73b1c28b4b42defa7fe8a61a6
Leo Semenenko <leo@gridpane.com>
2023-03-09 01:48:02 -0600

    --

--- .gpconfig/predeploy-server.sh
+++ .gpconfig/predeploy-server.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
+set -x
 # Server level access
 # - Runs before files have been deployed
 # - Runs as root user
 # - Runs from within site /.gpconfig dir
 env
-
 timeout 300 wget https://getcomposer.org/download/2.5.4/composer.phar
 timeout 300 chmod 755 ../wp-content/plugins/breakdance/plugin/vendor/bin/mozart
 export COMPOSER_ALLOW_SUPERUSER=1
