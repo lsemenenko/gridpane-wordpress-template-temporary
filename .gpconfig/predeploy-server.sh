917291b42fadff25b5ec540b338780a11978574e
Leo Semenenko <leo@gridpane.com>
2023-03-15 02:38:18 -0500

    ---

--- .gpconfig/predeploy-server.sh
+++ .gpconfig/predeploy-server.sh
@@ -5,8 +5,15 @@ set -x
 # - Runs as root user
 # - Runs from within site /.gpconfig dir
 env
-timeout 300 wget https://getcomposer.org/download/2.5.4/composer.phar
-timeout 300 chmod 755 ../wp-content/plugins/breakdance/plugin/vendor/bin/mozart
+#timeout 300 wget https://getcomposer.org/download/2.5.4/composer.phar
+#timeout 300 chmod 755 ../wp-content/plugins/breakdance/plugin/vendor/bin/mozart
+#export COMPOSER_ALLOW_SUPERUSER=1
+#timeout 300 php composer.phar update -n --working-dir=../wp-content/plugins/breakdance/plugin/
+#timeout 300 php composer.phar install -n --working-dir=../wp-content/plugins/breakdance/plugin/
+
+timeout 300 wget https://getcomposer.org/download/2.5.4/composer.phar -P /$GP_GIT_SITE_PATH
+timeout 300 chown $GP_GIT_SITE_USER /$GP_GIT_SITE_PATH/composer.phar
+timeout 300 chmod 755 $GP_GIT_SITE_PATH/wp-content/plugins/breakdance/plugin/vendor/bin/mozart
 export COMPOSER_ALLOW_SUPERUSER=1
-timeout 300 php composer.phar update -n --working-dir=../wp-content/plugins/breakdance/plugin/
-timeout 300 php composer.phar install -n --working-dir=../wp-content/plugins/breakdance/plugin/
+timeout 300 php composer.phar update -n --working-dir=$GP_GIT_SITE_PATH/wp-content/plugins/breakdance/plugin/
+timeout 300 php composer.phar install -n --working-dir=$GP_GIT_SITE_PATH/wp-content/plugins/breakdance/plugin/
